54f16fb5a57e86ab18278aecf366a0325c6ace09
Sakshidubey13 <sakshidubey1304@gmail.com>
2026-06-04 14:25:21 +0530

    ..

--- modules/server.js
+++ modules/server.js
@@ -4,7 +4,11 @@
 //sum(10, 30);
 
 //sencond way import and export for multiple value
-const sum = require("./app.js");
+//1st mmethod const app = require("./app.js");
+//app.sum(10, 20);
+//app.div(40, 20);
 
-app.sum(10, 20);
-app.div(40, 20);
+const { sum, div } = require("./app.js");
+
+sum(10, 20);
+div(40, 20);
