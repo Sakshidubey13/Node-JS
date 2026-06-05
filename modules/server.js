1d1855efd54e157513f0632dc888d6ee5ccf3edb
Sakshidubey13 <sakshidubey1304@gmail.com>
2026-06-05 17:23:10 +0530

    ..

--- modules/server.js
+++ modules/server.js
@@ -48,5 +48,12 @@ import fs from "fs";
 //    </html>`,
 //);
 
-
 fs.writeFileSync("bill.pdf","this is bill")
+
+
+//2. path module
+//that perfrom can change only  package.json =>  "type": "commonjs" this apply only
+//const path = require("path");
+//console.log(path.basename(__filename));
+
+
