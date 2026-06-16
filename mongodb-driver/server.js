e62d82e691041ea734f4b55df335f9a9925ddc68
Sakshidubey13 <sakshidubey1304@gmail.com>
2026-06-16 23:43:06 +0530

    ..

--- mongodb-driver/server.js
+++ mongodb-driver/server.js
@@ -4,4 +4,4 @@ const url = "mongodb://127.0.0.1:27017";   // MongoDB connection URL
 const client = new MongoClient(url);
 const dbName = "mydb";                     // Database name
 
-//step 2 
+//step 2  Create a Reusable DB Connection Function
