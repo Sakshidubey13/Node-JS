87be7896c1bfb42dedfb883bab575b0ccbbcb43e
Sakshidubey13 <sakshidubey1304@gmail.com>
2026-06-16 23:39:36 +0530

    ..

---
+++ mongodb-driver/server.js
@@ -0,0 +1,5 @@
+import { MongoClient } from "mongodb";
+
+const url = "mongodb://127.0.0.1:27017";   // MongoDB connection URL
+const client = new MongoClient(url);
+const dbName = "mydb";                     // Database name
