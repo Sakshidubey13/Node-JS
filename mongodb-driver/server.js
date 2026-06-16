5fc86345010afd95e8f4456661d29f2a200af727
Sakshidubey13 <sakshidubey1304@gmail.com>
2026-06-16 23:44:02 +0530

    ..

--- mongodb-driver/server.js
+++ mongodb-driver/server.js
@@ -1,7 +1,9 @@
 // step 1. import & connect mongodb server
 import { MongoClient } from "mongodb";
-const url = "mongodb://127.0.0.1:27017";   // MongoDB connection URL
+const url = "mongodb://127.0.0.1:27017"; // MongoDB connection URL
 const client = new MongoClient(url);
-const dbName = "mydb";                     // Database name
+const dbName = "mydb"; // Database name
 
 //step 2  Create a Reusable DB Connection Function
+
+export const connectDB = async () => {};
