876e68cdbf9af1cacd63c7b92f6b260b8d6e1d06
Sakshidubey13 <sakshidubey1304@gmail.com>
2026-06-27 23:21:28 +0530

    ..

--- mongodb/students/server.js
+++ mongodb/students/server.js
@@ -14,7 +14,7 @@ const connectDB = async () => {
 // flow of mongodb
 //database --> collection --> document(file) --> key : value
 
-//CURD operation using MongoDB Driver
+//CD operation using MongoDB Driver
 //1. Create
 const addStudent = async () => {
   const db = await connectDB();
