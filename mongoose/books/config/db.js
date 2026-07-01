09bd07e934b61bbaa22ccf2bda8bdd8aec2aae60
Sakshidubey13 <sakshidubey1304@gmail.com>
2026-07-01 15:25:51 +0530

    //

--- mongoose/books/config/db.js
+++ mongoose/books/config/db.js
@@ -1,1 +1,18 @@
 import mongoose from "mongoose";
+
+const url = "mongodb://127.0.0.1:27107/bookstore";//bookstore will become the  database name
+
+export const connectDB = async (req, res) => {
+  await mongoose.connect(url);
+};
+
+//try...catch..use
+
+//Que how to handle exception in javasecript ==> using try and catch
+//try{
+//    //expected error code
+//    }
+//catch(err){
+//    //err - > which error comes 
+//
+//}
