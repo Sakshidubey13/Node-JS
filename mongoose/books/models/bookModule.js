1776ad3d62b9353a2c6188757519cc222dc26bbb
Sakshidubey13 <sakshidubey1304@gmail.com>
2026-07-02 15:02:37 +0530

    ...

--- mongoose/books/models/bookModule.js
+++ mongoose/books/models/bookModule.js
@@ -1,3 +1,17 @@
 import mongoose from "mongoose";
 
-const bookSchema = new mongoose.Schema({},{})
+const bookSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    price: { type: Number, required: true },
+    descriptioin: { type: String },
+    category: { type: String },
+    publishedYear: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.module("Book", bookSchema);
