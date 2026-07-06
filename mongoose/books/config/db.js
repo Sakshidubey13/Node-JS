3bb5da9c99de3b5217c1296f61dbd81b2e2135dd
Sakshidubey13 <sakshidubey1304@gmail.com>
2026-07-06 13:36:49 +0530

    ..

--- mongoose/books/config/db.js
+++ mongoose/books/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const url = "mongodb://127.0.0.1:27107/bookstore"; //bookstore will become the  database name
 
 
-const connectDB = async (req, res) => {
+const connectDB = async () => {
   try {
     await mongoose.connect(url);
     console.log("MongoDB connected successfully !!");
