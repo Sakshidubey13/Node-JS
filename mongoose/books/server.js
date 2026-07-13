922cd4a0c4c442de41edae76facfc8bb94d4ffae
Sakshidubey13 <sakshidubey1304@gmail.com>
2026-07-13 22:34:55 +0530

    ..

--- mongoose/books/server.js
+++ mongoose/books/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import connectDB from "./config/db.js";
 import router from "./routes/bookRoute.js";
 
+
 const app = express();
 //whenever you want to work with json data , writ this line app.use(express.json())
 app.use(express.json()); //midleware -> its validate request before sending to the server
